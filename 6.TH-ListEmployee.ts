6577e8544c64d44402680014428e20bb2679f9f6
HieuT23511 <hieut23511@gmail.com>
2023-04-05 10:57:16 +0700

    ListEmployee

--- 6.TH-ListEmployee.ts
+++ 6.TH-ListEmployee.ts
@@ -7,11 +7,11 @@ enum Gender {
 class Employee {
     name: string;
     gender: Gender = Gender.OTHERS;
-    birthday?: Date;
+    birthday: Date;
     email: string;
-    phoneNumber: string;
+    phoneNumber?: string;
 
-    constructor(name: string, gender: Gender, birthday: Date, email: string, phoneNumber: string) {
+    constructor(name: string, gender: Gender, birthday: Date, email: string, phoneNumber?: string) {
         this.name = name;
         this.gender = gender;
         this.birthday = birthday;
@@ -20,9 +20,9 @@ class Employee {
     }
 }
 let employeeList: Employee[] = [];
-let employeee1 = new Employee('Chu Trong Hieu',Gender.MALE, new Date("1995-03-14"),'hieuct.work@gmail.com','0866511558')
-let employeee2 = new Employee('Chu Tron Hieu',Gender.FEMALE, new Date("1996-03-14"),'hieuct1.work@gmail.com','086651156')
-let employeee3 = new Employee('Chu Tro Hieu',Gender.OTHERS, new Date("1997-03-14"),'hieuct2.work@gmail.com','0866511554')
+let employeee1 = new Employee('Chu Trong Hieu',Gender.MALE, new Date("1995-03-14"),'hieuct.work@gmail.com')
+let employeee2 = new Employee('Chu Tron Hieu',Gender.FEMALE, new Date("1996-03-14"),'hieuct1.work@gmail.com')
+let employeee3 = new Employee('Chu Tro Hieu',Gender.OTHERS, new Date("1997-03-14"),'hieuct2.work@gmail.com')
 employeeList.push(employeee1,employeee2,employeee3);
 employeeList.forEach((employee:Employee) =>{console.log(employee)});
 
